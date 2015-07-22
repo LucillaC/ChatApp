a9a04e07355cf3225fd513cf667d442ca547f06c
LVC10 <LucillaC@users.noreply.github.com>
2015-07-22 13:07:38 -0700

    Updating based on tutorial

--- quickstart.js
+++ quickstart.js
@@ -7,10 +7,10 @@ if (!navigator.webkitGetUserMedia && !navigator.mozGetUserMedia) {
   alert('WebRTC is not available in your browser.');
 }
 
-// choose between one of our three pre-generated Access Tokens
-var accessTokens = {'alice': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImN0eSI6InR3aWxpby1zYXQ7dj0xIn0.eyJqdGkiOiJTSzc3MWM0ODg0YTUyYjQ3ZGEwNTY4ZDA1YjllNzhiNDRkLTE0Mzc1NDE4OTQiLCJpc3MiOiJTSzc3MWM0ODg0YTUyYjQ3ZGEwNTY4ZDA1YjllNzhiNDRkIiwic3ViIjoiQUM5ZDE4ZDcxOTYzNDJkMWRlMWNlNjE5MjNjNjU4MDJlMyIsIm5iZiI6MTQzNzU0MTg5NCwiZXhwIjoxNDM3NjI4Mjk0LCJncmFudHMiOlt7InJlcyI6Imh0dHBzOlwvXC9hcGkudHdpbGlvLmNvbVwvMjAxMC0wNC0wMVwvQWNjb3VudHNcL0FDOWQxOGQ3MTk2MzQyZDFkZTFjZTYxOTIzYzY1ODAyZTNcL1Rva2Vucy5qc29uIiwiYWN0IjpbIlBPU1QiXX0seyJyZXMiOiJzaXA6cXVpY2tzdGFydEBBQzlkMThkNzE5NjM0MmQxZGUxY2U2MTkyM2M2NTgwMmUzLmVuZHBvaW50LnR3aWxpby5jb20iLCJhY3QiOlsibGlzdGVuIiwiaW52aXRlIl19XX0.XM3wlCEXrukGUv8kQzVTPgYBE2y9O2HC7YIlh10fau8',
-                    'bob': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImN0eSI6InR3aWxpby1zYXQ7dj0xIn0.eyJqdGkiOiJTSzc3MWM0ODg0YTUyYjQ3ZGEwNTY4ZDA1YjllNzhiNDRkLTE0Mzc1NDI3MzIiLCJpc3MiOiJTSzc3MWM0ODg0YTUyYjQ3ZGEwNTY4ZDA1YjllNzhiNDRkIiwic3ViIjoiQUM5ZDE4ZDcxOTYzNDJkMWRlMWNlNjE5MjNjNjU4MDJlMyIsIm5iZiI6MTQzNzU0MjczMiwiZXhwIjoxNDM3NjI5MTMyLCJncmFudHMiOlt7InJlcyI6Imh0dHBzOlwvXC9hcGkudHdpbGlvLmNvbVwvMjAxMC0wNC0wMVwvQWNjb3VudHNcL0FDOWQxOGQ3MTk2MzQyZDFkZTFjZTYxOTIzYzY1ODAyZTNcL1Rva2Vucy5qc29uIiwiYWN0IjpbIlBPU1QiXX0seyJyZXMiOiJzaXA6Ym9iQEFDOWQxOGQ3MTk2MzQyZDFkZTFjZTYxOTIzYzY1ODAyZTMuZW5kcG9pbnQudHdpbGlvLmNvbSIsImFjdCI6WyJsaXN0ZW4iLCJpbnZpdGUiXX1dfQ.MjRTfsCIJuIJUBAsIPMBlHjAnBBQYgbb7uGbx9l5seU',
-                    'charlie': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImN0eSI6InR3aWxpby1zYXQ7dj0xIn0.eyJqdGkiOiJTSzc3MWM0ODg0YTUyYjQ3ZGEwNTY4ZDA1YjllNzhiNDRkLTE0Mzc1NDE4OTQiLCJpc3MiOiJTSzc3MWM0ODg0YTUyYjQ3ZGEwNTY4ZDA1YjllNzhiNDRkIiwic3ViIjoiQUM5ZDE4ZDcxOTYzNDJkMWRlMWNlNjE5MjNjNjU4MDJlMyIsIm5iZiI6MTQzNzU0MTg5NCwiZXhwIjoxNDM3NjI4Mjk0LCJncmFudHMiOlt7InJlcyI6Imh0dHBzOlwvXC9hcGkudHdpbGlvLmNvbVwvMjAxMC0wNC0wMVwvQWNjb3VudHNcL0FDOWQxOGQ3MTk2MzQyZDFkZTFjZTYxOTIzYzY1ODAyZTNcL1Rva2Vucy5qc29uIiwiYWN0IjpbIlBPU1QiXX0seyJyZXMiOiJzaXA6cXVpY2tzdGFydEBBQzlkMThkNzE5NjM0MmQxZGUxY2U2MTkyM2M2NTgwMmUzLmVuZHBvaW50LnR3aWxpby5jb20iLCJhY3QiOlsibGlzdGVuIiwiaW52aXRlIl19XX0.XM3wlCEXrukGUv8kQzVTPgYBE2y9O2HC7YIlh10fau8'};
+// generate an access token in the Twilio Account Portal - https://www.twilio.com/user/account/video/testing-tools
+var accessTokens = {'alice': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImN0eSI6InR3aWxpby1zYXQ7dj0xIn0.eyJqdGkiOiJTSzc3MWM0ODg0YTUyYjQ3ZGEwNTY4ZDA1YjllNzhiNDRkLTE0Mzc1OTU1NDAiLCJpc3MiOiJTSzc3MWM0ODg0YTUyYjQ3ZGEwNTY4ZDA1YjllNzhiNDRkIiwic3ViIjoiQUM5ZDE4ZDcxOTYzNDJkMWRlMWNlNjE5MjNjNjU4MDJlMyIsIm5iZiI6MTQzNzU5NTU0MCwiZXhwIjoxNDM3NjgxOTQwLCJncmFudHMiOlt7InJlcyI6Imh0dHBzOlwvXC9hcGkudHdpbGlvLmNvbVwvMjAxMC0wNC0wMVwvQWNjb3VudHNcL0FDOWQxOGQ3MTk2MzQyZDFkZTFjZTYxOTIzYzY1ODAyZTNcL1Rva2Vucy5qc29uIiwiYWN0IjpbIlBPU1QiXX0seyJyZXMiOiJzaXA6YWxpY2VAQUM5ZDE4ZDcxOTYzNDJkMWRlMWNlNjE5MjNjNjU4MDJlMy5lbmRwb2ludC50d2lsaW8uY29tIiwiYWN0IjpbImxpc3RlbiIsImludml0ZSJdfV19.toFSoLx1NoGAkG_kspobD4GGZYr2VeQd4nuv0tTSrtw',
+                    'bob': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImN0eSI6InR3aWxpby1zYXQ7dj0xIn0.eyJqdGkiOiJTSzc3MWM0ODg0YTUyYjQ3ZGEwNTY4ZDA1YjllNzhiNDRkLTE0Mzc1OTU1NzAiLCJpc3MiOiJTSzc3MWM0ODg0YTUyYjQ3ZGEwNTY4ZDA1YjllNzhiNDRkIiwic3ViIjoiQUM5ZDE4ZDcxOTYzNDJkMWRlMWNlNjE5MjNjNjU4MDJlMyIsIm5iZiI6MTQzNzU5NTU3MCwiZXhwIjoxNDM3NjgxOTcwLCJncmFudHMiOlt7InJlcyI6Imh0dHBzOlwvXC9hcGkudHdpbGlvLmNvbVwvMjAxMC0wNC0wMVwvQWNjb3VudHNcL0FDOWQxOGQ3MTk2MzQyZDFkZTFjZTYxOTIzYzY1ODAyZTNcL1Rva2Vucy5qc29uIiwiYWN0IjpbIlBPU1QiXX0seyJyZXMiOiJzaXA6Ym9iQEFDOWQxOGQ3MTk2MzQyZDFkZTFjZTYxOTIzYzY1ODAyZTMuZW5kcG9pbnQudHdpbGlvLmNvbSIsImFjdCI6WyJsaXN0ZW4iLCJpbnZpdGUiXX1dfQ.z_kYeKW0QB85g0vuoSa6quFwqknIIi6sHPNChayTl_Q',
+                    'charlie': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImN0eSI6InR3aWxpby1zYXQ7dj0xIn0.eyJqdGkiOiJTSzc3MWM0ODg0YTUyYjQ3ZGEwNTY4ZDA1YjllNzhiNDRkLTE0Mzc1OTU1OTciLCJpc3MiOiJTSzc3MWM0ODg0YTUyYjQ3ZGEwNTY4ZDA1YjllNzhiNDRkIiwic3ViIjoiQUM5ZDE4ZDcxOTYzNDJkMWRlMWNlNjE5MjNjNjU4MDJlMyIsIm5iZiI6MTQzNzU5NTU5NywiZXhwIjoxNDM3NjgxOTk3LCJncmFudHMiOlt7InJlcyI6Imh0dHBzOlwvXC9hcGkudHdpbGlvLmNvbVwvMjAxMC0wNC0wMVwvQWNjb3VudHNcL0FDOWQxOGQ3MTk2MzQyZDFkZTFjZTYxOTIzYzY1ODAyZTNcL1Rva2Vucy5qc29uIiwiYWN0IjpbIlBPU1QiXX0seyJyZXMiOiJzaXA6Y2hhcmxpZUBBQzlkMThkNzE5NjM0MmQxZGUxY2U2MTkyM2M2NTgwMmUzLmVuZHBvaW50LnR3aWxpby5jb20iLCJhY3QiOlsibGlzdGVuIiwiaW52aXRlIl19XX0.l_z9OUtGYESs5frSarztqgGMKiERIr0Er_fs53J9bpM'};
 var userName = prompt('Please enter your name - alice, bob or charlie.', 'alice');
 var accessToken = accessTokens[userName];
 
@@ -27,20 +27,19 @@ endpoint.listen().then(
 function endpointConnected() {
   document.getElementById('invite-controls').style.display = 'block';
   log("Connected to Twilio. Listening for incoming Invites as '" + endpoint.address + "'");
-
-  endpoint.on('invite', function (invite) {
+};
+endpoint.on('invite', function (invite) {
     log('Incoming invite from: ' + invite.from);
     invite.accept().then(conversationStarted);
   });
-
-  // bind button to create conversation
+};
+// bind button to create conversation
   document.getElementById('button-invite').onclick = function () {
     var inviteTo = document.getElementById('invite-to').value;
-
-    if (activeConversation) {
+      if (activeConversation) {
       // add a participant
       activeConversation.invite(inviteTo);
-    } else {
+     } else {
       // create a conversation
       var options = {};
       if (previewMedia) {
@@ -55,11 +54,12 @@ function endpointConnected() {
       );
     }
   };
+
 };
 
 // conversation is live
 function conversationStarted(conversation) {
-  log('In an active Conversation');
+  log("In an active Conversation");
   activeConversation = conversation;
   // draw local video, if not already previewing
   if (!previewMedia) {
@@ -82,7 +82,6 @@ function conversationStarted(conversation) {
     activeConversation = null;
   });
 };
-
 //  local video preview
 document.getElementById('button-preview').onclick = function () {
   if (!previewMedia) {
@@ -99,7 +98,6 @@ document.getElementById('button-preview').onclick = function () {
     );
   };
 };
-
 // activity log
 function log(message) {
   document.getElementById('log-content').innerHTML = message;
